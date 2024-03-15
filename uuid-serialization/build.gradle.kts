2665b992f86ed666dcfdac6f4f065fadd17f9ef2
subochev <caffeine.mgn@gmail.com>
2024-03-14 17:23:44 -0700

    fix gradle version

--- uuid-serialization/build.gradle.kts
+++ uuid-serialization/build.gradle.kts
@@ -1,9 +1,10 @@
 
+import org.jetbrains.kotlin.konan.target.HostManager
 import pw.binom.publish.allTargets
 import pw.binom.publish.applyDefaultHierarchyBinomTemplate
 
 plugins {
-    id("org.jetbrains.kotlin.multiplatform")
+    kotlin("multiplatform")
     id("maven-publish")
     id("kotlinx-serialization")
 }
@@ -14,12 +15,48 @@ if (pw.binom.Target2.ANDROID_JVM_SUPPORT) {
 }
 apply<pw.binom.plugins.ConfigPublishPlugin>()
 kotlin {
-    allTargets()
-    applyDefaultHierarchyBinomTemplate()
+    if (HostManager.hostIsMac) {
+        macosX64()
+        macosArm64()
+        iosX64()
+        iosArm64()
+        iosSimulatorArm64()
+        tvosArm64()
+        tvosSimulatorArm64()
+        watchosArm32()
+        watchosArm64()
+        watchosDeviceArm64()
+        watchosSimulatorArm64()
+        watchosX64()
+    }
+    jvm()
+    linuxX64()
+    linuxArm64()
+    mingwX64()
+    js {
+        browser()
+        nodejs()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+    }
+    wasmWasi {
+        nodejs()
+    }
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+
+//    applyDefaultHierarchyTemplate()
+//    allTargets()
+//    applyDefaultHierarchyBinomTemplate()
+    applyDefaultHierarchyTemplate()
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api(kotlin("stdlib-common"))
                 api(project(":uuid"))
                 api("org.jetbrains.kotlinx:kotlinx-serialization-core:${pw.binom.Versions.KOTLINX_SERIALIZATION_VERSION}")
             }
--- uuid/build.gradle.kts
+++ uuid/build.gradle.kts
@@ -1,8 +1,9 @@
+import org.jetbrains.kotlin.konan.target.HostManager
 import pw.binom.publish.allTargets
 import pw.binom.publish.applyDefaultHierarchyBinomTemplate
 
 plugins {
-    id("org.jetbrains.kotlin.multiplatform")
+    kotlin("multiplatform")
     id("maven-publish")
 }
 
@@ -11,20 +12,57 @@ if (pw.binom.Target2.ANDROID_JVM_SUPPORT) {
     apply<pw.binom.plugins.AndroidSupportPlugin>()
 }
 kotlin {
-    allTargets()
-    applyDefaultHierarchyBinomTemplate()
+
+    if (HostManager.hostIsMac) {
+        macosX64()
+        macosArm64()
+        iosX64()
+        iosArm64()
+        iosSimulatorArm64()
+        tvosArm64()
+        tvosSimulatorArm64()
+        watchosArm32()
+        watchosArm64()
+        watchosDeviceArm64()
+        watchosSimulatorArm64()
+        watchosX64()
+    }
+    jvm()
+    linuxX64()
+    linuxArm64()
+    mingwX64()
+    js {
+        browser()
+        nodejs()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+    }
+    wasmWasi {
+        nodejs()
+    }
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+
+//    applyDefaultHierarchyTemplate()
+
+//    applyDefaultHierarchyBinomTemplate()
+    applyDefaultHierarchyTemplate()
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(kotlin("stdlib-common"))
-            }
+        commonTest.dependencies {
+            api(kotlin("test-common"))
+            api(kotlin("test-annotations-common"))
         }
-        val commonTest by getting {
-            dependencies {
-                api(kotlin("test-common"))
-                api(kotlin("test-annotations-common"))
-            }
-        }
+//        val commonTest by getting {
+//            dependencies {
+//                api(kotlin("test-common"))
+//                api(kotlin("test-annotations-common"))
+//            }
+//        }
 //        useDefault()
     }
 }
